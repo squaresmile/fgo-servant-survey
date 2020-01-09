05bc7c444d0a9758fb2bbc4e8c6b230782b6d152
squaresmile <squaresmile@protonmail.com>
2020-01-08 20:30:16 -0500

    Update deployment settings
    Format python files with black

--- app.py
+++ app.py
@@ -9,70 +9,83 @@ import plotly.graph_objs as go
 player_df = pd.read_csv("data/merged_df.csv", index_col=0)
 servant_df = pd.read_csv("data/servant - manual.csv")
 
-servant_classes = list(servant_df['Class'].unique())
-servant_availability = list(servant_df['Availability'].unique())
-player_types = list(player_df['Money Spent'].unique())
+servant_classes = list(servant_df["Class"].unique())
+servant_availability = list(servant_df["Availability"].unique())
+player_types = list(player_df["Money Spent"].unique())
 player_types.sort()
 
-dash_classes = [{'label': servant_class, 'value': servant_class} for servant_class in servant_classes]
-dash_availability = [{'label': availability, 'value': availability} for availability in servant_availability]
-dash_types = [{'label': type, 'value': type} for type in player_types]
+dash_classes = [
+    {"label": servant_class, "value": servant_class}
+    for servant_class in servant_classes
+]
+dash_availability = [
+    {"label": availability, "value": availability}
+    for availability in servant_availability
+]
+dash_types = [{"label": type, "value": type} for type in player_types]
 
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div(children=[
-    html.H1(children='FGO NA Summer Servant Survey'),
-
-    html.Div(children='Servant Class'),
-
-    dcc.Checklist(
-        id='class_checklist',
-        options=dash_classes,
-        labelStyle={'display': 'inline-block'},
-        values=servant_classes
-    ),
-
-    html.Div(children='Servant Availability'),
-
-    dcc.Checklist(
-        id='availability_checklist',
-        options=dash_availability,
-        labelStyle={'display': 'inline-block'},
-        values=servant_availability
-    ),
-
-    html.Div(children='Spending Amount'),
-
-    dcc.Checklist(
-        id='type_checklist',
-        options=dash_types,
-        labelStyle={'display': 'inline-block'},
-        values=player_types
-    ),
-
-    dcc.Graph(id='fgo_servant_data')
-], className="container"
+app.layout = html.Div(
+    children=[
+        html.H1(children="FGO NA Summer Servant Survey"),
+        html.Div(children="Servant Class"),
+        dcc.Checklist(
+            id="class_checklist",
+            options=dash_classes,
+            labelStyle={"display": "inline-block"},
+            values=servant_classes,
+        ),
+        html.Div(children="Servant Availability"),
+        dcc.Checklist(
+            id="availability_checklist",
+            options=dash_availability,
+            labelStyle={"display": "inline-block"},
+            values=servant_availability,
+        ),
+        html.Div(children="Spending Amount"),
+        dcc.Checklist(
+            id="type_checklist",
+            options=dash_types,
+            labelStyle={"display": "inline-block"},
+            values=player_types,
+        ),
+        dcc.Graph(id="fgo_servant_data"),
+    ],
+    className="container",
 )
 
+
 @app.callback(
-    dash.dependencies.Output('fgo_servant_data', 'figure'),
-    [dash.dependencies.Input('class_checklist', 'values'),
-     dash.dependencies.Input('availability_checklist', 'values'),
-     dash.dependencies.Input('type_checklist', 'values')])
+    dash.dependencies.Output("fgo_servant_data", "figure"),
+    [
+        dash.dependencies.Input("class_checklist", "values"),
+        dash.dependencies.Input("availability_checklist", "values"),
+        dash.dependencies.Input("type_checklist", "values"),
+    ],
+)
 def update_graph(chosen_class, chosen_availability, chosen_type):
-    chosen_servants = list(servant_df[servant_df['Class'].isin(chosen_class) & servant_df['Availability'].isin(chosen_availability)]['Servant'])
-    summary = player_df[player_df['Money Spent'].isin(chosen_type)][chosen_servants].sum().replace(False, 0)
+    chosen_servants = list(
+        servant_df[
+            servant_df["Class"].isin(chosen_class)
+            & servant_df["Availability"].isin(chosen_availability)
+        ]["Servant"]
+    )
+    summary = (
+        player_df[player_df["Money Spent"].isin(chosen_type)][chosen_servants]
+        .sum()
+        .replace(False, 0)
+    )
 
     return {
-            'data': [
-                go.Pie(
-					labels = list(summary.index),
-					values = list(summary),
-                    direction = "clockwise"
-                )
-            ]
-        }
+        "data": [
+            go.Pie(
+                labels=list(summary.index), values=list(summary), direction="clockwise"
+            )
+        ]
+    }
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
--- create player datasets.py
+++ create player datasets.py
@@ -1,70 +1,105 @@
 import pandas as pd
 import re
 
-def main():
-    servant_survey_df = pd.read_csv("data/Google Sheets csv/SSR Actual 2018 Dec (respuestas).csv", mangle_dupe_cols=True)
 
-    #These records are Paid Gacha or 100-300 but record values in the columns for F2P
+def main():
+    servant_survey_df = pd.read_csv(
+        "data/Google Sheets csv/SSR Actual 2018 Dec (respuestas).csv",
+        mangle_dupe_cols=True,
+    )
+
+    # These records are Paid Gacha or 100-300 but record values in the columns for F2P
     servant_survey_df = servant_survey_df.drop([111, 1029, 2395])
 
     splitted_df = {}
 
-    player_type_list = [p_type for p_type in servant_survey_df['How much money have you used in-game?'].unique() if not pd.isnull(p_type) and p_type != '']
+    player_type_list = [
+        p_type
+        for p_type in servant_survey_df[
+            "How much money have you used in-game?"
+        ].unique()
+        if not pd.isnull(p_type) and p_type != ""
+    ]
     for money_type in player_type_list:
-        splitted_df[money_type] = servant_survey_df[servant_survey_df['How much money have you used in-game?'] == money_type]
+        splitted_df[money_type] = servant_survey_df[
+            servant_survey_df["How much money have you used in-game?"] == money_type
+        ]
 
-    PROPER_COLUMNS_NAME = ['Time', 'Money Spent', 'Saber', 'Archer', 'Lancer', 'Rider', 'Caster', 'Assassin', 'Berserker', 'Ruler', 'Avenger']
+    PROPER_COLUMNS_NAME = [
+        "Time",
+        "Money Spent",
+        "Saber",
+        "Archer",
+        "Lancer",
+        "Rider",
+        "Caster",
+        "Assassin",
+        "Berserker",
+        "Ruler",
+        "Avenger",
+    ]
 
-    #Each player type servant data are recorded in different columns for different player type.
-    #Eg: F2P Sabers in 'SSR Sabers'; Paid Gacha Sabers in 'SSR Sabers.1' and so on
-    #Therefore, if NA columns are removed, only columns of the relevant player type remain
+    # Each player type servant data are recorded in different columns for different player type.
+    # Eg: F2P Sabers in 'SSR Sabers'; Paid Gacha Sabers in 'SSR Sabers.1' and so on
+    # Therefore, if NA columns are removed, only columns of the relevant player type remain
     for player_type, player_df in splitted_df.items():
-        splitted_df[player_type] = player_df.dropna(axis = 1, how = 'all').iloc[:,:11]
+        splitted_df[player_type] = player_df.dropna(axis=1, how="all").iloc[:, :11]
         splitted_df[player_type].columns = PROPER_COLUMNS_NAME
 
     servant_class_list = {}
 
-    #Use whale dataset to make sure we don't miss any servant
+    # Use whale dataset to make sure we don't miss any servant
     for servant_class in PROPER_COLUMNS_NAME[2:11]:
-        servant_class_list[servant_class] = list(splitted_df['501 USD and onwards'][servant_class].dropna().unique())
+        servant_class_list[servant_class] = list(
+            splitted_df["501 USD and onwards"][servant_class].dropna().unique()
+        )
 
-    #Get servant list for each class
+    # Get servant list for each class
     for servant_class, servant_list in servant_class_list.items():
-        servant_class_list[servant_class] = list({str(ssr).strip() for item in servant_list for ssr in item.split(',')})
+        servant_class_list[servant_class] = list(
+            {str(ssr).strip() for item in servant_list for ssr in item.split(",")}
+        )
         servant_class_list[servant_class].sort()
 
-    #Create servants' columms containing TRUE or FALSE
+    # Create servants' columms containing TRUE or FALSE
     for player_type, player_df in splitted_df.items():
         for servant_class in servant_class_list:
             for servant in servant_class_list[servant_class]:
-                splitted_df[player_type][servant] = player_df[servant_class].str.contains(re.escape(servant), na = False)
+                splitted_df[player_type][servant] = player_df[
+                    servant_class
+                ].str.contains(re.escape(servant), na=False)
 
     merged_survey_df = pd.DataFrame()
 
     for player_type, player_df in splitted_df.items():
-        splitted_df[player_type]['Money Spent'] = player_type
+        splitted_df[player_type]["Money Spent"] = player_type
         merged_survey_df = merged_survey_df.append(splitted_df[player_type])
 
-    merged_survey_df = merged_survey_df.drop(columns = PROPER_COLUMNS_NAME[2:11])
+    merged_survey_df = merged_survey_df.drop(columns=PROPER_COLUMNS_NAME[2:11])
 
     PROPER_PLAYER_NAME = {
-        'More than 1USD but less than 101 Monthly': '$1-100 Monthly',
-        '101USD and onwards, but less than 301 Monthly': '$100-300 Monthly',
-        '501 USD and onwards': '$500- Monthly',
-        '301USD and onwards but less than 501 Montlhy': '$300-500 Monthly'
+        "More than 1USD but less than 101 Monthly": "$1-100 Monthly",
+        "101USD and onwards, but less than 301 Monthly": "$100-300 Monthly",
+        "501 USD and onwards": "$500- Monthly",
+        "301USD and onwards but less than 501 Montlhy": "$300-500 Monthly",
     }
     merged_survey_df = merged_survey_df.replace(PROPER_PLAYER_NAME)
 
-    merged_survey_df.to_csv("data/merged_df.csv", index_label='index')
+    merged_survey_df.to_csv("data/merged_df.csv", index_label="index")
 
     # for player_type, player_df in splitted_df.items():
     #     player_df.to_csv("data/{}.csv".format(player_type))
 
-    servant_class_table = [(servant, servant_class) for servant_class in servant_class_list for servant in servant_class_list[servant_class]]
-    servant_df = pd.DataFrame(servant_class_table, columns=['Servant', 'Class'])
+    servant_class_table = [
+        (servant, servant_class)
+        for servant_class in servant_class_list
+        for servant in servant_class_list[servant_class]
+    ]
+    servant_df = pd.DataFrame(servant_class_table, columns=["Servant", "Class"])
 
-    #This data set doesn't contain limitted data though so that has to be inputted manually at the moment
-    servant_df.to_csv('data/servant.csv', index=False)
+    # This data set doesn't contain limitted data though so that has to be inputted manually at the moment
+    servant_df.to_csv("data/servant.csv", index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
