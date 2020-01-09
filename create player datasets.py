23e9beeb53323e6cb728d3e71eaddaa890a675f6
squaresmile <squaresmile@protonmail.com>
2020-01-08 22:28:07 -0500

    Add temp 2020-01 data

--- create player datasets.py
+++ create player datasets.py
@@ -4,13 +4,14 @@ import pandas as pd
 
 def main():
     servant_survey_df = pd.read_csv(
-        "data/Google Sheets csv/SSR NA Actual FEB 2019 (respuestas) - Respuestas de formulario 1.csv",
+        "data/Google Sheets csv/SSRs actuales Enero 2020 (respuestas) - Respuestas de formulario 1.csv",
         mangle_dupe_cols=True,
     )
-
+    print(servant_survey_df.shape)
     # These records are Paid Gacha or 100-300 but record values in the columns for F2P
     # cols_to_drop = [111, 1029, 2395]
     # servant_survey_df = servant_survey_df.drop(cols_to_drop)
+    print(servant_survey_df["How much money have you used in-game?"].value_counts())
 
     splitted_df = {}
 
@@ -21,6 +22,8 @@ def main():
         ].unique()
         if not pd.isnull(p_type) and p_type != ""
     ]
+    for t in player_type_list:
+        print(t)
     for money_type in player_type_list:
         splitted_df[money_type] = servant_survey_df[
             servant_survey_df["How much money have you used in-game?"] == money_type
@@ -38,21 +41,23 @@ def main():
         "Berserker",
         "Ruler",
         "Avenger",
+        "Alter Ego",
+        "Foreigner"
     ]
 
     # Each player type servant data are recorded in different columns for different player type.
     # Eg: F2P Sabers in 'SSR Sabers'; Paid Gacha Sabers in 'SSR Sabers.1' and so on
     # Therefore, if NA columns are removed, only columns of the relevant player type remain
     for player_type, player_df in splitted_df.items():
-        splitted_df[player_type] = player_df.dropna(axis=1, how="all").iloc[:, :11]
+        splitted_df[player_type] = player_df.dropna(axis=1, how="all").iloc[:, :len(PROPER_COLUMNS_NAME)]
         splitted_df[player_type].columns = PROPER_COLUMNS_NAME
 
     servant_class_list = {}
 
     # Use whale dataset to make sure we don't miss any servant
-    for servant_class in PROPER_COLUMNS_NAME[2:11]:
+    for servant_class in PROPER_COLUMNS_NAME[2:]:
         servant_class_list[servant_class] = list(
-            splitted_df["501 USD and onwards"][servant_class].dropna().unique()
+            splitted_df["High Spender (201-300~ USD per month)"][servant_class].dropna().unique()
         )
 
     # Get servant list for each class
@@ -76,16 +81,18 @@ def main():
         splitted_df[player_type]["Money Spent"] = player_type
         merged_survey_df = merged_survey_df.append(splitted_df[player_type])
 
-    merged_survey_df = merged_survey_df.drop(columns=PROPER_COLUMNS_NAME[2:11])
+    merged_survey_df = merged_survey_df.drop(columns=PROPER_COLUMNS_NAME[2:])
 
     PROPER_PLAYER_NAME = {
-        "More than 1USD but less than 101 Monthly": "$1-100 Monthly",
-        "101USD and onwards, but less than 301 Monthly": "$100-300 Monthly",
-        "501 USD and onwards": "$500- Monthly",
-        "301USD and onwards but less than 501 Montlhy": "$300-500 Monthly",
+        "Occasional Spender (1-50~ USD per month)": "$1-50 Monthly",
+        "Moderate Spender (51-100~ USD per month)": "$51-100 Monthly",
+        "Low-High Spender (101-200~ USD per month)": "$101-200 Monthly",
+        "High Spender (201-300~ USD per month)": "$201-300 Monthly",
+        "High-High Spender (301USD and onwards per month)": "$301- Monthly",
     }
     merged_survey_df = merged_survey_df.replace(PROPER_PLAYER_NAME)
-
+    print(merged_survey_df.shape)
+    print(merged_survey_df["Money Spent"].value_counts())
     merged_survey_df.to_csv("data/merged_df.csv", index_label="index")
 
     # for player_type, player_df in splitted_df.items():
